3f38931c2d03f14715989616f7578f8d87ea9518
Steffin <steffin.codes@gmail.com>
2020-06-16 20:23:00 +0530

    Styled some stuff ✨ Added Backlinks 🖇️

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,185 @@
 import Link from "next/link";
 import fs from "fs";
+import Head from "next/head";
 
 const Home = ({ slug }) => (
-  <div>
-    Lists of posts:
-    {slug.map(slug => {
-      return (
-        <div key={slug}>
-          <Link href={"/blog/" + slug}>
-            <a>{"/blog/" + slug}</a>
-          </Link>
-        </div>
-      );
-    })}
+  <div className="container">
+    <Head>
+      <title>Blog List</title>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>"
+      />
+    </Head>
+
+    <main>
+      <h1 className="title">Listing <a href="https://github.com/steffin-codes/devlog/tree/master/posts">.md</a> files</h1>
+
+      <p className="description">
+        A <strong>Next.js</strong> powered blog with headless CMS hosted on github pages written
+        using markdown :)
+      </p>
+
+      <div className="grid">
+        {slug.map((slug) => {
+          return (
+            // <div key={slug}>
+            <Link href={"/blog/" + slug}>
+              <a className="card">
+                <h3>{titleCase(slug.split("-").slice(1).join(" "))}</h3>
+              </a>
+            </Link>
+            // </div>
+          );
+        })}
+      </div>
+    </main>
+
+    <footer>Made with 🦄</footer>
+
+    <style jsx>{`
+      .container {
+        min-height: 100vh;
+        padding: 0 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      ::selection {
+        background: var(--accent-color);
+        color: var(--bg-color);
+      }
+
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid var(--bg-color);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      .title a {
+        color: var(--accent-color);
+        text-decoration: none;
+      }
+
+      .title a:hover,
+      .title a:focus,
+      .title a:active {
+        text-decoration: underline;
+      }
+
+      .title {
+        margin: 0;
+        line-height: 1.15;
+        font-size: 4rem;
+      }
+
+      .title,
+      .description {
+        text-align: center;
+      }
+
+      .description {
+        line-height: 1.5;
+        font-size: 1.5rem;
+      }
+
+      code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-size: 1.1rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+
+      .grid {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        max-width: 800px;
+        margin-top: 3rem;
+      }
+
+      .card {
+        margin: 1rem;
+        flex-basis: 45%;
+        padding: 1.5rem;
+        text-align: left;
+        color: inherit;
+        text-decoration: none;
+        border: 1px solid var(--bg-color);
+        border-radius: 10px;
+        transition: color 0.15s ease, border-color 0.15s ease;
+      }
+
+      .card:hover,
+      .card:focus,
+      .card:active {
+        color: var(--accent-color);
+        border-color: var(--accent-color);
+      }
+
+      .card h3 {
+        margin: 0 0 1rem 0;
+        font-size: 1.5rem;
+      }
+
+      .card p {
+        margin: 0;
+        font-size: 1.25rem;
+        line-height: 1.5;
+      }
+
+      .logo {
+        height: 1em;
+      }
+
+      @media (max-width: 600px) {
+        .grid {
+          width: 100%;
+          flex-direction: column;
+        }
+      }
+    `}</style>
+
+    <style jsx global>{`
+      @import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;1,300;1,400&display=swap");
+      :root {
+        --bg-color: #eaeaea;
+        --font-color: black;
+        --accent-color: #7a490b;
+      }
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: Raleway, Roboto, Helvetica Neue, sans-serif;
+      }
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
   </div>
 );
 
@@ -20,9 +187,19 @@ export const getStaticProps = async () => {
   const files = fs.readdirSync("posts");
   return {
     props: {
-      slug: files.map(filename => filename.replace(".md", ""))
-    }
+      slug: files.map((filename) => filename.replace(".md", "")),
+    },
   };
 };
+
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
 
 export default Home;
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -16,32 +16,57 @@ const Post = ({ htmlString, data }) => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>"
         />
       </Head>
+      <ul class="nav">
+        <li>
+          <a alt="home" href="/">🏠</a>
+        </li>
+        <li>
+          <a alt="blog list" href="/blog">📚</a>
+        </li>
+      </ul>
       <div dangerouslySetInnerHTML={{ __html: htmlString }} />
       <style jsx global>{`
-        @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;1,300;1,400&display=swap');
+        @import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,600;1,300;1,400&display=swap");
         :root {
           --bg-color: #eaeaea;
           --font-color: black;
           --accent-color: #7a490b;
         }
+        * {
+          box-sizing: border-box;
+        }
         html,
         body {
           padding: 0;
-          margin: 1em auto;
-          width: 80vw;
+          margin: 0;
           font-family: Raleway, Roboto, Helvetica Neue, sans-serif;
         }
-        pre{
+        body {
+          margin: 1em auto;
+          width: 80vw;
+        }
+        pre {
           background: #eee;
           font-family: consolas;
           padding: 1em;
           border-radius: 1em;
+          overflow-wrap: break-word;
+          word-wrap: break-word;
+          hyphens: auto;
         }
-
-        * {
-          box-sizing: border-box;
+        .nav{
+          display: flex;
+          justify-content: flex-start;
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
         }
-
+        .nav li{
+          padding-right: 1em;
+        }
+        a{
+          text-decoration: none;
+        }
       `}</style>
     </div>
   );
@@ -50,16 +75,16 @@ const Post = ({ htmlString, data }) => {
 export const getStaticPaths = async () => {
   const files = fs.readdirSync("posts");
   console.log("files: ", files);
-  const paths = files.map(filename => ({
+  const paths = files.map((filename) => ({
     params: {
-      slug: filename.replace(".md", "")
-    }
+      slug: filename.replace(".md", ""),
+    },
   }));
   console.log("paths: ", paths);
 
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 };
 
@@ -75,8 +100,8 @@ export const getStaticProps = async ({ params: { slug } }) => {
   return {
     props: {
       htmlString,
-      data: parsedMarkdown.data
-    }
+      data: parsedMarkdown.data,
+    },
   };
 };
 
